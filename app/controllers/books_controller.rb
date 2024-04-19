536f07a86c91ad1445d321c92ad7508570e23a10
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-19 08:47:41 +0000

    [Start]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  before_action :is_matching_author, only: [:edit, :update, :destroy]
+  
+  def index
+    @books = Book.all
+    @new_book = Book.new
+    @user = current_user
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end 
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end 
+  
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to books_path
+  end 
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end 
+  
+  def is_matching_author?
+    book = Book.find(params[:id])
+    unless current_user.id == book.user_id
+      redirect_to books_path
+    end 
+  end 
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :is_matching_user?, only: [:edit, :update]
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end 
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @new_book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @user = current_user
+    @new_book = Book.new
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end 
+  
+  def is_matching_user?
+    user = User.find(params[:id])
+    unless currene_user.id == user.id
+      redirect_to user_path(current_user.id)
+    end 
+  end
+end
