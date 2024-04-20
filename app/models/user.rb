9acef2ae409b4175e868be21c2b96f96703391bf
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-20 02:26:40 +0000

    [Add]残りレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: { minimum:2, maximum:20 }
   validates :introduction, length: { maximum:50 }
   
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image', content_type: 'image/jpeg')
+    else
+      profile_image.variant(resize_to_limit: [width, height]).processed
+    end 
+  end 
 end
