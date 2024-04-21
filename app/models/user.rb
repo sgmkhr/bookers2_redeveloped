0176cf19ff59188a98e862f477e665e9f51663ba
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-21 10:06:50 +0000

    [Add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   has_many :books, dependent: :destroy
-  #has_many :favorites, dependent: :destroy
-  #has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   
   validates :name, uniqueness: true, length: { minimum:2, maximum:20 }
-  #validates :introduction, length: { maximum:50 }
+  validates :introduction, length: { maximum:50 }
   
   def get_profile_image(width, height)
     unless profile_image.attached?
@@ -19,6 +24,18 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image', content_type: 'image/jpeg')
     end 
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end 
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end 
+  
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end 
+  
+  def following?(user)
+    followings.include?(user)
   end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :users, only: [:edit, :show, :index, :update]
+  resources :users, only: [:edit, :show, :index, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
