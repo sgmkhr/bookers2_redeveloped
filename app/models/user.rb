58943b002a4de6507c5e9d5960cea038719b682a
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-21 09:03:38 +0000

    [Fix]スペルミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,18 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
+  #has_many :favorites, dependent: :destroy
+  #has_many :book_comments, dependent: :destroy
   
   validates :name, uniqueness: true, length: { minimum:2, maximum:20 }
-  validates :introduction, length: { maximum:50 }
+  #validates :introduction, length: { maximum:50 }
   
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image', content_type: 'image/jpeg')
-    else
-      profile_image.variant(resize_to_limit: [width, height]).processed
     end 
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end 
 
 end
