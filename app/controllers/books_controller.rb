ea710d8f0d570c349e945c5668c9ef8afcedb181
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-22 05:55:56 +0000

    [Add]閲覧数表示機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,9 @@ class BooksController < ApplicationController
     @user = @book.user
     @new_book = Book.new
     @book_comment = BookComment.new
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end 
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   
+  has_many :view_counts, dependent: :destroy
+  
   validates :name, uniqueness: true, length: { minimum:2, maximum:20 }
   validates :introduction, length: { maximum:50 }
   
