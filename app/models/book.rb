d8ca8bff3111407b6d3c987805f15f10526b5529
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-22 03:45:31 +0000

    [Add]検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,7 +7,22 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum:200 }
 
- def favorited_by?(user)
-   favorites.exists?(user_id: user.id)
- end
-end
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+ 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      Book.where("title LIKE?","%#{word}%")
+    else
+      Book.all
+    end
+  end
+  
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end 
   
+  get 'search' => 'searches#search', as: 'search'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
