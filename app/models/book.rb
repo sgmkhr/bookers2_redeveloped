3054be469cd4d9592e72a25733bf69631b336874
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-28 01:58:41 +0000

    [Add]投稿数推移チャート

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,27 +1,30 @@
 class Book < ApplicationRecord
   include Notifiable
-  
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :view_counts, dependent: :destroy
-  
+
   has_many :notifications, as: :notifiable, dependent: :destroy
-  
+
   #has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
-  
+
   validates :title, presence: true
   validates :body, presence: true, length: { maximum:200 }
-  
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
+  scope :created_days_ago, -> (n) { where(created_at: n.day.ago.all_day) }
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
- 
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -33,20 +36,24 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%' + content + '%')
     end
   end
-  
+
   after_create do
     records = user.followers.map do |follower|
       notifications.new(user_id: follower.id)
     end
     Notification.import records
   end
-  
+
   def notification_message
     "フォローしている#{user.name}さんが#{title}を投稿しました"
-  end 
-  
+  end
+
   def notification_path
     book_path(self)
-  end 
-    
-end 
+  end
+  
+  def self.past_week_count
+    (1..6).map { |n| created_days_ago(n).count }.reverse.push(created_today.count)
+  end
+
+end
